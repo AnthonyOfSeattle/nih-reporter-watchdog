8c85c598e5579e1ccbef84e5398cb701cb77dee3
Anthony Valente <valenta4@uw.edu>
2025-03-13 22:35:47 -0400

    Add fancier functions for yearly downloads

--- scripts/download_projects.py
+++ scripts/download_projects.py
@@ -2,6 +2,7 @@ import copy
 import datetime
 import logging
 import json
+import itertools
 import os
 import requests
 from pprint import pprint
@@ -90,8 +91,109 @@ def download_items_for_date(from_date, force=False):
         json.dump(items, dest, indent=4)
 
 
+def get_items_for_date_range(from_date, to_date):
+    # Attempt to download data
+    logger.info(
+        f"Downloading data from {from_date.isoformat()}"
+        f" to {to_date.isoformat()}"
+    )
+    criteria = {
+        "date_added": {
+            "from_date": from_date.isoformat(),
+            "to_date": to_date.isoformat()
+        },
+    }
+    items = get_all_items(criteria)
+    if not items:
+        logger.info(
+            "No items found for date range"
+        )
+        return
+
+    # Group by date_added and send to files
+    logger.info(
+        f"Found {len(items)} items for date range"
+    )
+    items = sorted(
+        items,
+        key=lambda x: (
+            datetime.datetime.fromisoformat(x["date_added"]).date(),
+            x["appl_id"]
+        )
+    )
+    grouped_items = itertools.groupby(
+        items,
+        lambda x: datetime.datetime.fromisoformat(x["date_added"]).date()
+    )
+    for date, group in grouped_items:
+        group = list(group)
+        logger.info(
+            f"Writing {len(group)} items for {date}"
+        )
+        dest_dir = os.path.join(
+            "data",
+            "json",
+            "projects",
+            f"year_added={date.strftime('%Y')}",
+            f"month_added={date.strftime('%m')}"
+        )
+        dest_filename = f"projects_added_{date.strftime('%Y_%m_%d')}.json"
+        with open(os.path.join(dest_dir, dest_filename), "w") as dest:
+            json.dump(group, dest, indent=4)
+
+    
+
+def get_date_of_first_refresh(year):
+    """Walks through dates to find first date where a refresh happened"""
+    logger.info(f"Searching for date of first refresh in {year}")
+    cur_date = datetime.datetime(year=year, month=1, day=1)
+    while cur_date < datetime.datetime(year=year + 1, month=1, day=1):
+        to_date = (
+            cur_date +
+                datetime.timedelta(days=1) -
+                datetime.timedelta(microseconds=1)
+        )
+        payload = {
+            "criteria": {
+                "date_added": {
+                    "from_date": cur_date.isoformat(),
+                    "to_date": to_date.isoformat()
+                }
+
+            },
+            "limit": 1
+        }
+
+        response = requests.post(
+            os.path.join(REPORTER_API_URL, "projects", "search"),
+            json=payload
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        if data["meta"]["total"]:
+            return cur_date
+        cur_date += datetime.timedelta(days=1)
+
+
+def get_data_for_year(year):
+    """Walks through a year week by week to download data"""
+    from_date = get_date_of_first_refresh(year)
+    while from_date < datetime.datetime(year=year + 1, month=1, day=1):
+        # Download data a week at a time
+        to_date = (
+            from_date +
+                datetime.timedelta(days=7) -
+                datetime.timedelta(microseconds=1)
+        )
+        get_items_for_date_range(from_date, to_date)
+        from_date += datetime.timedelta(days=7)
+
+
 if __name__ == "__main__":
-    query_datetime = datetime.datetime.fromisoformat("2022-01-01")
-    while query_datetime < datetime.datetime.fromisoformat("2025-04-01"):
-        download_items_for_date(query_datetime, force=True)
-        query_datetime += datetime.timedelta(days=1)
+    get_data_for_year(2024)
+    get_data_for_year(2025)
+    #query_datetime = datetime.datetime.fromisoformat("2015-01-01")
+    #while query_datetime < datetime.datetime.fromisoformat("2016-01-01"):
+    #    download_items_for_date(query_datetime, force=True)
+    #    query_datetime += datetime.timedelta(days=1)
